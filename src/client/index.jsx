7818c0edd9b1308022adcb5871c00cdc200d786a
K-Dot911 <kozlovskij.gleb@mail.ru>
2023-03-06 22:06:11 +0300

    Revert "feat: added preparing app"
    This reverts commit 766321aacac7ddb79041b61661cf77b883e58057.

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Header } from "../shared/Header";
-import { App } from "../App";
 
 window.addEventListener('load', () => {
-    ReactDOM.hydrate(<App />, document.getElementById('react-root'));
+    ReactDOM.hydrate(<Header />, document.getElementById('react-root'));
 });
 
